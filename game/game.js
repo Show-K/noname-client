ce3fe58f6425310f1ab5a168fa773bd5e6cc5d14
Show-K Adot <1143158498x@gmail.com>
2021-12-19 16:39:13 +0800

    Test

--- game/game.js
+++ game/game.js
@@ -44793,12 +44793,12 @@
 				delete lib.arenaReady;
 				//Load custom extension start
 				var addtional_extention_names=[
-					['在线更新', true],
-					['拖拽读取', true],
+					['在线更新',true],
+					['拖拽读取',false]
 				];
 				var need_reload=false;
 				for(var i=0;i<addtional_extention_names.length;i++){
-					if(!lib.config.extensions.contains(addtional_extention_names[i][0])&&addtional_extention_names[i][1]){
+					if(!lib.config.extensions.contains(addtional_extention_names[i][0])){
 						var need_reload=true;
 						lib.config.extensions.add(addtional_extention_names[i][0]);
 						game.saveConfig('extensions',lib.config.extensions);
